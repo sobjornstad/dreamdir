ac4bddb054da464573c44befebef4ff111102d73
Soren I. Bjornstad <contact@sorenbjornstad.com>
2016-01-02 09:53:55 -0600

    formatting in dotplot script; add 2016 to range

--- scripts/date-dotplot.R
+++ scripts/date-dotplot.R
@@ -2,15 +2,18 @@ R_TRANSFER_FILE = 'dotplot-data.csv'
 OUTPUT_LOCATION = '../graphs/'
 
 minYear = 2011
-maxYear = 2015
+maxYear = 2016
 
 library(ggplot2)
 library(scales)
 
 data <- read.csv(R_TRANSFER_FILE, sep=',', header=TRUE, stringsAsFactors=FALSE)
 for (year in minYear:maxYear) {
-    data$month = factor(data$month, labels=month.abb)
-    qplot(month,day, data=data[data$year==year,], geom='point', size=numdreams, xlab="Month", ylab="Day", main=paste("Dream log entries in", year))
+    data$month <- factor(data$month, labels=month.abb)
+    qplot(month,day, data=data[data$year==year,],
+          geom='point', size=numdreams,
+          xlab="Month", ylab="Day",
+          main=paste("Dream log entries in", year))
     fname <- paste(OUTPUT_LOCATION, year, "-recall-dotplot.svg", sep='')
     ggsave(filename=fname, width=8, height=5)
 }
