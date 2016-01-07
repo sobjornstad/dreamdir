b4437e08b86609520ec9542afee076f32d4b290f
Soren I. Bjornstad <contact@sorenbjornstad.com>
2016-01-07 08:23:58 -0600

    add show-tagged option to dr
    (said option opens the found dreams in vim arglist)

--- scripts/RegeneratePlots.sh
+++ scripts/RegeneratePlots.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+. ~/bin/common.sh
+
+reason="Please run this script from the dreamdir root directory."
+ensure '-d scripts' "$reason"
+ensure '-d graphs'  "$reason"
+ensure '-f dr'      "$reason"
+pushd scripts
 
 python ./date-dotplot.py
 R --vanilla < ./date-dotplot.R
-rm dotplot-data.csv
 
+rm dotplot-data.csv
 rm -f Rplots.pdf
+popd
