6140eb33fa4a38dee1e7d5b6619cf01d1555b6b2
Soren I. Bjornstad <contact@sorenbjornstad.com>
2016-06-01 09:49:48 -0500

    add shellcheck tests to pre-commit hook

--- tests/pre-commit-hook.sh
+++ tests/pre-commit-hook.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# requires shellcheck: http://www.shellcheck.net/
+echo -n "Running shellcheck..."
+shellcheck dr || exit 1
+echo "OK"
+
+# requires BATS: https://github.com/sstephenson/bats
 echo "Running tests..."
 tests/test_dr || exit 1
+echo "OK"
 
+# requires GCC or equivalent compiler with c99 support
 echo -e "\nRebuilding C code to check for errors..."
-cd scripts
+{
+cd scripts || exit 2
 make clean
 make || exit 1
-cd - > /dev/null
+}
+echo "OK"
 
-echo -e "\nEverything completed successfully."
+echo -e "Everything completed successfully."
